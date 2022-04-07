e4aa11cd63c416d89fd3b50d5c67424b76968d69
Ya-maz <vochko50@gmail.com>
2022-04-07 12:19:57 +0300

    add localStorage, media from os theme. Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { IconSunrise, IconSunset } from './IconSun';
+import { useTheme } from './hooks/useTheme';
 
 function App() {
+  const { theme, setTheme } = useTheme();
+  console.log(theme);
+  const switchToLight = () => setTheme('light');
+  const switchToDark = () => setTheme('dark');
   return (
-    <div className={'App min-h-screen'}>
+    <div className={'app min-h-screen'}>
       <div className="flex justify-end">
-        <button className="font-bold py-2 px-4 rounded">Light</button>
-        <button className="font-bold py-2 px-4 rounded">Dark</button>
+        <button
+          onClick={switchToLight}
+          className="button m-1 font-bold py-2 px-4 rounded"
+        >
+          Light
+        </button>
+        <button
+          onClick={switchToDark}
+          className="button m-1 font-bold py-2 px-4 rounded"
+        >
+          Dark
+        </button>
       </div>
-      <div className="flex flex-col min-h-fit">
+      <div className="flex flex-col">
         <div className="flex justify-center items-center">
-        <i class="bi bi-sunrise" style={{fontSize: "4rem"}}></i>
+          <IconSunrise />
+          <IconSunset />
         </div>
         <div>
           <form action="">
-            <input type="text" placeholder="text" />
-            <button className="font-bold py-2 px-4 rounded">Submit</button>
+            <input
+              type="text"
+              placeholder="text"
+              className="input rounded p-2 min-h-fit"
+            />
+            <button className="m-2 bg-sky-600 font-bold py-2 px-4 rounded">
+              Submit
+            </button>
           </form>
         </div>
-        <div>
-
-        </div>
+        <div></div>
       </div>
     </div>
   );
